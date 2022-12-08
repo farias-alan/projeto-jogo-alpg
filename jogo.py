1a3a2b68cbdf2ce1e78f9e2bcf50a988fa180818
Alan Farias <aaof2@aluno.ifal.edu.br>
2022-12-08 18:14:55 -0300

    feat: adição de personagem

--- jogo.py
+++ jogo.py
@@ -5,27 +5,34 @@ pygame.init()
 x = 1280
 y = 720
 
-screem = pygame.display.set_mode((x,y))
+screem = pygame.display.set_mode((x, y))
 pygame.display.set_caption("jogo de tiro")
 
-bg = pygame.image.load("imagens/imagemcidade.png")
-bg = pygame.transform.scale(bg, (x,y))
+bg = pygame.image.load("imagemcidade.png")
+bg = pygame.transform.scale(bg, (x, y))
 rodando = True
+
+# Carro de Policía
+personagem = pygame.image.load('policia_arma.png')
+personagem = pygame.transform.scale(personagem, (150, 150))
+
+posicao_personagem_x = 10
+posicao_personagem_y = 500
 
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            
+
             rodando = False
-    screem.blit(bg, (0,0))
+    screem.blit(bg, (0, 0))
 
     rel_x = x % bg.get_rect().width
-    screem.blit(bg, (rel_x - bg.get_rect().width,0))
+    screem.blit(bg, (rel_x - bg.get_rect().width, 0))
     if rel_x < 1280:
-        screem.blit(bg,(rel_x, 0))
-    
+        screem.blit(bg, (rel_x, 0))
+
     x -= 1
 
-
+    screem.blit(personagem, (posicao_personagem_x, posicao_personagem_y))
 
     pygame.display.update()
