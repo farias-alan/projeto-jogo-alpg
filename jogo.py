e4e4ed73dafd9f724703a4b2eddd5bd79e3621a5
Alan Farias <aaof2@aluno.ifal.edu.br>
2022-12-11 21:32:46 -0300

    feat: movimentação personagem

--- jogo.py
+++ jogo.py
@@ -5,7 +5,7 @@ pygame.init()
 x = 1280
 y = 720
 
-screem = pygame.display.set_mode((x, y))
+tela = pygame.display.set_mode((x, y))
 pygame.display.set_caption("jogo de tiro")
 
 bg = pygame.image.load("imagemcidade.png")
@@ -13,43 +13,35 @@ bg = pygame.transform.scale(bg, (x, y))
 rodando = True
 
 # Carro de Policía
-personagem = pygame.image.load('policia_arma.png')
-personagem = pygame.transform.scale(personagem, (150, 150))
+carro = pygame.image.load('carro.png')
+carro = pygame.transform.scale(carro, (150, 150))
 
-posicao_personagem_x = 10
-posicao_personagem_y = 500
+posicao_carro_x = 50
+posicao_carro_y = 515
 
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-
             rodando = False
-    screem.blit(bg, (0, 0))
+
+    tela.blit(bg, (0, 0))
 
     rel_x = x % bg.get_rect().width
-    screem.blit(bg, (rel_x - bg.get_rect().width, 0))
+    tela.blit(bg, (rel_x - bg.get_rect().width, 0))
     if rel_x < 1280:
-        screem.blit(bg, (rel_x, 0))
+        tela.blit(bg, (rel_x, 0))
 
+    # Movimentação Carro
+    movimento = pygame.key.get_pressed()
+    if movimento[pygame.K_UP] and posicao_carro_y > 510:
+        posicao_carro_y -= 1
+    if movimento[pygame.K_DOWN] and posicao_carro_y < 580:
+        posicao_carro_y += 1
+
+    # Velocidade Tela
     x -= 1
 
-    screem.blit(personagem, (posicao_personagem_x, posicao_personagem_y))
+    # Imagem
+    tela.blit(carro, (posicao_carro_x, posicao_carro_y))
 
     pygame.display.update()
-
-'''
-código para movimentação do personagem:
-
-velocidade = 10
-
-   comandos = pygame.key.get_pressed()
-    if comandos[pygame.K_UP]:
-        y -= velocidade
-    if comandos[pygame.K_DOWN]:
-        y += velocidade
-    if comandos[pygame.K_RIGHT]:
-        y += velocidade
-    if comandos[pygame.K_LEFT]:
-        x -= velocidade
-        
-'''
